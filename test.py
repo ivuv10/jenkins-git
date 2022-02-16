72f2f2f65ee382a29dc9d7e1fd4f350ec9374fb8
ivuv10 <nivgal@gmail.com>
2022-02-16 23:32:09 +0200

    first update

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print   ('my name is niv')
+while True:
+    try:
+        name = input ("What is your name ? ").strip()
+        if name.startswith("A"):
+            print("message for A")
+            break       
+        elif name.startswith("N"):
+            print("message for N")
+            break
+        else:
+            print("Sorry, my only purpose is to talk to N and A")
+    except ValueError:
+        print ("Sorry, my only purpose is to talk to N and A")
